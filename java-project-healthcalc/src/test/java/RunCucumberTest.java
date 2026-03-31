d6a4dab9abca7c882ff45b7a723f20baf811610b
Juan Ruiz <juaanruiiz86@gmail.com>
2026-03-31 21:42:58 +0200

    fix: arreglar código bmi_full.feature y BMIFullSteps.java
    Arreglar el código al idioma correcto, @Dado,@Cuando,@Entonces.
    Corregido fallos de minúsculas, mayúsculas, comas, puntos para la correcta ejecución de los test con RunCucumberTest.java

---
+++ java-project-healthcalc/src/test/java/RunCucumberTest.java
@@ -0,0 +1,20 @@
+
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+// 1. Apuntamos a la carpeta de resources donde está el .feature
+@SelectPackages("")
+// 2. Apuntamos al paquete exacto donde está BMIFullSteps.java
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
+public class RunCucumberTest { 
+} 
+
